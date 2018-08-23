b6b21808f1c499f0ecf02d5567100021436c6f01
Nandhini Govindasamy <nandinigovi143@gmail.com>
2018-08-23 16:30:26 +0530

    retrofit call for check user and register

--- android/app/src/main/java/com/samayu/scaction/service/SCAService.java
+++ android/app/src/main/java/com/samayu/scaction/service/SCAService.java
@@ -5,7 +5,9 @@ import com.samayu.scaction.dto.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * Created by NandhiniGovindasamy on 8/22/18.
@@ -14,7 +16,10 @@ import retrofit2.http.POST;
 public interface SCAService {
 
     @POST("user/register")
-    public Call<User> registerDevice(@Body CreateUser createUser);
+    public Call<User> registerNewUser(@Body CreateUser createUser);
+
+    @GET("user/checkUser/{fbUser}")
+    public Call<User> checkUser(@Path("fbUser") String fbUser);
 
 
 }
