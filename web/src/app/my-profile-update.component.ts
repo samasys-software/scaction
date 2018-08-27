08175362b1e73a0a9cfb7241ebb05d20cd9130fd
samayuadmin <samasys@gmail.com>
2018-08-26 20:37:05 -0400

    Added Ajax Binding for Dropdown and City Search

--- web/src/app/my-profile-update.component.ts
+++ web/src/app/my-profile-update.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectorRef, OnInit, Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
@@ -7,6 +7,7 @@ import { environment } from '../environments/environment';
 import { Country } from './types/country';
 import { Router, Routes, ActivatedRoute } from '@angular/router';
 import { UserHolderService } from './user-holder.service';
+import { Capability } from 'protractor';
 
 
 
@@ -20,7 +21,7 @@ export class MyProfileUpdateComponent implements OnInit {
   public model: any;
   public modelCountry: any;
   public cities: any;
-  public countries: Country[];
+  public countries: Observable<Country[]>;
   public fbUserId: any;
   public fbName: any;
   public fbEmail: any;
@@ -34,15 +35,7 @@ export class MyProfileUpdateComponent implements OnInit {
         : this.cities.filter(v => v['name'].toLowerCase().indexOf(term.toLowerCase()) > -1).map(v => v['name']).slice(0, 10))
     )
 
-    searchCountry = (text$: Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map(term => term.length < 2 ? []
-        : this.countries.filter(v => v['name'].toLowerCase().indexOf(term.toLowerCase()) > -1).map(v => v['name']).slice(0, 10))
-    )
-
-    ngOnInit() {
+  ngOnInit() {
       console.log('Initializing Profile Update Component ');
       this.userHolder.currentMessage.subscribe( message => { 
         this.fbEmail = message['fbEmail'];
@@ -59,8 +52,13 @@ export class MyProfileUpdateComponent implements OnInit {
 
     this.httpClient.get(environment.apiUrl + 'global/countries' ).subscribe((res) => {
       if (res != null) {
-        this.countries = Object.values(res).map(v => new Country(v));
-        /*this.chRef.detectChanges();*/
+        var tmp: Country[] = [];
+        for(let key in res){
+          var country = new Country(res[key]);
+          tmp.push( country );
+        }
+        this.countries = of(tmp).pipe();
+        
       }
 
       else {
@@ -75,6 +73,22 @@ export class MyProfileUpdateComponent implements OnInit {
   });
 
 
+ 
+    }
+    setCountry(value: string ){
+      this.httpClient.get(environment.apiUrl + 'global/cities/'+value ).subscribe((res) => {
+        if (res != null) {
+          this.cities = res;
+        }
+        else {
+          this.cities = null;
+        }
+      },
+    (error) => {
+      this.cities = null;
+      // this.chRef.detectChanges();
+    });
+  
     }
 
     constructor(private httpClient: HttpClient, private userHolder: UserHolderService, private route: ActivatedRoute ,
