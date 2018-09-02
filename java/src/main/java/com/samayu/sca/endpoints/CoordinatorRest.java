db6becd23028bf9b3ac03dc7f0b523ac18281456
samayuadmin <samasys@gmail.com>
2018-09-01 22:59:20 -0400

    Added Casting Call Find

---
+++ java/src/main/java/com/samayu/sca/endpoints/CoordinatorRest.java
@@ -0,0 +1,23 @@
+package com.samayu.sca.endpoints;
+
+import com.samayu.sca.businessobjects.CastingCall;
+
+import com.samayu.sca.service.DataAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import java.util.List;
+
+public class CoordinatorRest {
+
+    @Autowired
+    DataAccessService dataAccessService;
+
+    @GetMapping(path = "/castingcalls/{userId}")
+    public ResponseEntity<List<CastingCall>> getMyCastingCalls(@PathVariable("userId") long userId) {
+        return ResponseEntity.ok(dataAccessService.findCastingCallsByUser(userId));
+    }
+}
