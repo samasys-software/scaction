6d9f9d4a79e5bff01270d1eb37a0679cf80409a2
samayuadmin <samasys@gmail.com>
2018-09-02 00:16:33 -0400

    Added path

--- java/src/main/java/com/samayu/sca/endpoints/CoordinatorRest.java
+++ java/src/main/java/com/samayu/sca/endpoints/CoordinatorRest.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
 
+@RestController
+@RequestMapping(path="/coordinator")
 public class CoordinatorRest {
 
     @Autowired
